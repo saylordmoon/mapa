52f55ff484b2f7c611cef8e33db734b3af188d63
Rafael <queteimportaNSA@tuculo.com>
2014-05-01 23:58:29 -0500

    taking the text off

--- js/migraciones.js
+++ js/migraciones.js
@@ -4,14 +4,10 @@ function Migraciones(paper,x,y,width)
 	var migraciones = paper.set();
 
 	var llegadas 	= paper.image("img/llegadas.svg",x,y,width,width);
-	var llegadasTxt = paper.text(100, 110, "llegadas").attr({fill: '#ff0000'});
-	llegadasTxt.transform("s2.5,2.5");
 	var salidas  	= paper.image("img/salidas.svg",width + spaceBetween ,y ,width ,width);
-	// vae salidasTxt = paper.
 
 	migraciones.push(llegadas);
-	migraciones.push(llegadasTxt);
-	// migraciones.push(salidas);
+	migraciones.push(salidas);
 	
 
 	return migraciones;
