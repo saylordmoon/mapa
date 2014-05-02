5a28cd1177d353c4f158326cd62de830ec1d1ab1
Rafael <queteimportaNSA@tuculo.com>
2014-05-02 00:28:20 -0500

    adding text to menuitems

--- js/menu.js
+++ js/menu.js
@@ -9,17 +9,24 @@ function generateMenu(paper,x,y,width,items,callback)
 	for ( key in items )
 	{
 		var menuitem  		= paper.image("img/img2.png", x, ycont ,width,width);
+		var menuitemtext	= paper.text(x, ycont+width ,items[key]);
+		menuitemtext.scale(2);
 		menuitem.node.id 	= key;
+		menuitem.attr({cursor:'pointer'});
+
+		var value = items[key];
 
 		menu.push(menuitem);
 
 		ycont += width + spaceBetween;
-
-		var value = items[key];
-		callback(x,y,key,value);
-
-		console.log(key);
 	}
+
+	menu.click (function() 
+	{ 
+		console.log(this.node.id);
+		callback(this.node.id,"");
+	}); 
+
 
 	return menu;
 
